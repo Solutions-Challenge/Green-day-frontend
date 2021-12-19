443733cbd4ffdbf2e66553e3748936780b2d6f3f
Albert Lee <allilee@ucsc.edu>
2021-12-19 12:10:02 -0800

    Added some authentication functions

--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -1,36 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import auth from '@react-native-firebase/auth';
+import React, { useState, useEffect, useContext } from 'react';
 
-export default function AuthScreen() {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
-  // Handle user state changes
-  function onAuthStateChanged(user: any) {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
 
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  if (initializing) return null;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAz1kUNeaduc9_wh6_md3QduHKPMix25w4",
+  authDomain: "greenday-6aba2.firebaseapp.com",
+  projectId: "greenday-6aba2",
+  storageBucket: "greenday-6aba2.appspot.com",
+  messagingSenderId: "15765189134",
+  appId: "1:15765189134:web:a540ee84d8012dd1a4425f",
+  measurementId: "G-04MVDFPLWP"
+};
 
-  if (!user) {
-    return (
-      <View>
-        <Text>Login</Text>
-      </View>
-    );
-  }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-  return (
-    <View>
-      <Text>Welcome {user.email}</Text>
-    </View>
-  );
-}
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
