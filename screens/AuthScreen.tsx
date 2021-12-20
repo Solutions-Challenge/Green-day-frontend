62ea2c0cf9448e75abc06a432f70c9c03ca67f5f
Albert Lee <allilee@ucsc.edu>
2021-12-19 16:15:15 -0800

    Added the rest of the authentication functions

--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import {auth} from '../api/firebase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
@@ -9,8 +9,30 @@ const provider = new GoogleAuthProvider();
 const AuthScreen = () => {
   var email = "yester1324@gmail.com";
   var password = "abcd1234";
-  
 
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -25,11 +47,14 @@ const AuthScreen = () => {
       console.log("User isn't logged in");
     }
   });
+  
+  signOut(auth);
   var user;
   if (auth.currentUser != null)
     user = auth.currentUser.email;
   else
     user = "User not logged in";
+  
   
   return (
     <View style={{justifyContent:'center', alignItems:'center', flex:1}}>
